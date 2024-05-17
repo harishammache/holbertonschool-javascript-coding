7897abfebdb1db6d7968b834b38dbe6ad5d7f509
harishammache <hammache_haris@hotmail.fr>
2024-05-17 08:58:41 +0000

    task 0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+function FileContent (filePath) {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
+}
+
+const filePath = process.argv[2];
+FileContent(filePath);
