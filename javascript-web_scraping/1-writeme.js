a3cd505af39b69c5d2c3112d91820f7f77dd05c2
harishammache <hammache_haris@hotmail.fr>
2024-05-17 09:05:41 +0000

    task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filepath = process.argv[2];
+const str = process.argv[3];
+
+fs.writeFile(filepath, str, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
