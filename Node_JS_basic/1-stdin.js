ddd056a8512e76a7e64c2ff84d0d787b20ede2ca
harishammache <hammache_haris@hotmail.fr>
2024-05-24 19:17:00 +0000

    change

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,8 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
 process.stdin.on('data', (data) => {
-  const input = data.trim();
-
-  if (input !== '') {
-    process.stdout.write(`Your name is: ${input}\n`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-  }
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
